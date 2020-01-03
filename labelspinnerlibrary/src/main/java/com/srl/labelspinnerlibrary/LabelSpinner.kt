d360f9f587724bf7a1b43c85c3c24d086949e1bc
vimal <vimal@qaptive.co.in>
2020-01-03 15:32:18 +0530

    spinner layout

---
+++ labelspinnerlibrary/src/main/java/com/srl/labelspinnerlibrary/LabelSpinner.kt
@@ -0,0 +1,26 @@
+package com.srl.labelspinnerlibrary
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.widget.RelativeLayout
+
+class LabelSpinner : RelativeLayout {
+
+    constructor(context: Context) : super(context) {
+        init(context, null,null)
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init(context, attrs,null)
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(context, attrs, defStyle) {
+        init(context, attrs,defStyle)
+    }
+
+    fun init(context: Context, attrs: AttributeSet?, defStyle: Int?)
+    {
+        View.inflate(context,R.layout.lable_spinner_layout,this)
+    }
+}
